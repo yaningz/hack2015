b3c2a0feed5d8f1d16ba3191f28d5a9007dc3649
Yaning Zhang <yzhang17@mit.edu>
2015-09-19 13:59:11 -0400

    Facebook Oauth eventually maybe

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+
+	'facebookAuth' : {
+    'clientID'      : '1744755022480266',
+    'clientSecret'  : '91f83f8115bed9cf9b03176f18b58479',
+    'callbackURL'   : '/auth/facebook/callback'
+	},
+
+};
