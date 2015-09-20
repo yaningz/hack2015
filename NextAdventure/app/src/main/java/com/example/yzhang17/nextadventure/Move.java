191361c5973733c5f734f897d96920e15152c2a3
Yaning Zhang <yzhang17@mit.edu>
2015-09-19 21:22:58 -0400

    Switching to NextAdventure

---
+++ NextAdventure/app/src/main/java/com/example/yzhang17/nextadventure/Move.java
@@ -0,0 +1,31 @@
+package com.example.yzhang17.nextadventure;
+
+import java.util.Random;
+
+/**
+ * Created by yzhang17 on 9/19/2015.
+ */
+public class Move {
+    private final double accuracy;
+    private final String name;
+    private final int power;
+
+    Move(String name, double success, int power){
+        this.accuracy = success;
+        this.name = name;
+        this.power = power;
+    }
+
+    /**
+     * If hit is successful, deals damage within 10% of power
+     * @return value of damage dealt by this move
+     */
+    public int makeMove(){
+        Random r = new Random();
+        if(r.nextFloat() <= accuracy) {
+            return (int)((r.nextFloat()- .5) * power * .1 + power);
+        }
+        return 0;
+    }
+
+}
