6408b761e21ac33345ad33672801fc293335d4f2
Cynthia Lo <cyntlo@mit.edu>
2015-09-20 03:30:55 -0400

    Added extortion to Trainer

--- NextAdventure/app/src/main/java/com/example/yzhang17/nextadventure/Trainer.java
+++ NextAdventure/app/src/main/java/com/example/yzhang17/nextadventure/Trainer.java
@@ -2,6 +2,7 @@ package com.example.yzhang17.nextadventure;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
+import java.util.Random;
 
 /**
  * Created by yzhang17 on 9/19/2015.
@@ -10,15 +11,17 @@ public abstract class Trainer {
     private final String name;
     private final String location;
     private final List<Nextie> underlings;
+    private int techcash;
     private int currentUnderling = 0;
-    public Trainer(String name, String location, List<Nextie> underlings){
+    public Trainer(String name, String location, List<Nextie> underlings, int maxTechcash){
         this.name = name;
         this.location = location;
         List<Nextie> temp = new ArrayList<Nextie>();
         Collections.copy(underlings, temp);
         Collections.shuffle(temp);
         this.underlings = Collections.unmodifiableList(temp);
-    }
+        this.techcash = (new Random()).nextInt(maxTechcash);
+}
 
     public String introduction(){
         return "Hi, my name is " + name + ". Welcome to the " + location +". Let's battle!";
@@ -34,7 +37,11 @@ public abstract class Trainer {
         return null;
     }
 
-    public String lose(){ return "Oh no!  I lost!"; }
+    /**
+     *
+     * @return Amount of money dropped for beating this trainer
+     */
+    public int lose(){ return techcash; }
 
     public boolean canAttack(){
         return !underlings.get(currentUnderling).isDead();
